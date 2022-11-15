53074c782d90c2b12e0a3d5262d11edb6ddb9321
Le Cong Vu <Le Cong Vu@MSI>
2022-11-15 23:52:05 +0700

    update

---
+++ bai4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace bai4
+{
+    class Program
+    {
+
+        static bool soCP(int n)
+        {
+            int sqr = (int)Math.Sqrt(n);
+            if (sqr * sqr == n)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("\n Nhap vao so can kiem tra: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (soCP(n))
+            {
+                Console.WriteLine("\n So {0} la so chinh phuong.", n);
+            }
+            else
+            {
+                Console.WriteLine("\n So {0} khong phai la so chinh phuong.", n);
+            }
+
+        }
+    }
+}
